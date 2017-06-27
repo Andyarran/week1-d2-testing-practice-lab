76a0e79eb20dc9e98a2396921542e103e5aae97e
Andrew Weir <aweir@europe.com>
2017-06-27 15:20:06 +0100

    initial commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,40 @@
+def test_return_10()
+
+end
+
+def test_add()
+
+end
+
+def test_subtract()
+
+end
+
+def test_multiply()
+
+end
+
+def test_divide()
+
+end
+
+def test_length_of_string()
+
+end
+
+def test_join_string()
+
+end
+
+def test_add_string_as_number()
+
+end
+
+def test_number_to_full_name()
+
+end
+
+def test_substring()
+
+end
+
